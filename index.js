6112a36cfd0486d94937f3cf4435acb95a0edbe6
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-06-08 14:09:17 +0600

    complete module 78

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -9,6 +10,23 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized access' })
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'Unauthorized access' })
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 
 
@@ -27,43 +45,115 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-  const menuCollections = client.db('bistroDb').collection('menu')
-  const reviewCollections = client.db('bistroDb').collection('reviews')
-  const cartCollections = client.db('bistroDb').collection('carts')
+    const menuCollections = client.db('bistroDb').collection('menu');
+    const reviewCollections = client.db('bistroDb').collection('reviews');
+    const cartCollections = client.db('bistroDb').collection('carts');
+    const usersCollections = client.db('bistroDb').collection('users');
 
-  app.get('/menu', async(req, res)=>{
-    const result = await menuCollections.find().toArray();
-    res.send(result)
-  });
-  app.get('/reviews', async(req, res)=>{
-    const result = await reviewCollections.find().toArray();
-    res.send(result)
-  });
 
-  // cart collection apis
-  app.get('/carts', async(req, res)=>{
-    const email = req.query.email;
-    if(!email){
-      res.send([])
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollections.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(404).send({ error: true, message: 'forbidden messsage' })
+      }
+      next()
     }
-      const query = {email : email};
+
+    // users related appis
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollections.find().toArray();
+      res.send(result)
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollections.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user alreday exists' })
+      }
+      const result = await usersCollections.insertOne(user);
+      res.send(result);
+    });
+
+    // security layer : verifyJWt
+    // email same
+    // check admin
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+       return res.send({ admin: false })
+      }
+
+      const query = { email: email };
+      const user = await usersCollections.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result)
+    })
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollections.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    // menu relataed Apis
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result)
+    });
+    // review related apis
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollections.find().toArray();
+      res.send(result)
+    });
+
+    // cart collection apis
+    app.get('/carts', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'Forbidden access' })
+      }
+
+      const query = { email: email };
       const result = await cartCollections.find(query).toArray();
       res.send(result)
-    
-  })
-  app.post('/carts', async(req, res)=>{
-    const item = req.body;
-    const result = await cartCollections.insertOne(item);
-    res.send(result)
-  });
 
-  app.delete('/carts/:id', async(req, res)=>{
-    const id = req.params.id;
-    console.log(id)
-    const query = {_id : new ObjectId(id) }
-    const result = await cartCollections.deleteOne(query);
-    res.send(result)
-  })
+    })
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartCollections.insertOne(item);
+      res.send(result)
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollections.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -76,10 +166,10 @@ async function run() {
 run();
 
 
-app.get('/', (req, res)=>{
-    res.send('Restaurent is Open')
+app.get('/', (req, res) => {
+  res.send('Restaurent is Open')
 });
 
-app.listen(port, ()=>{
-    console.log(`server is Running on Port ${port}`)
+app.listen(port, () => {
+  console.log(`server is Running on Port ${port}`)
 })
